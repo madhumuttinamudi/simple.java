5e3c8565101836bf0cca5862de418732e7781c60
madhumuttinamudi <108851547+madhumuttinamudi@users.noreply.github.com>
2022-07-07 10:09:17 +0530

    first commit

---
+++ ifExample.java
@@ -0,0 +1,10 @@
+public class ifExample{
+	public static void main(String[] args){
+		//defining an 'age' variable
+		int age = 17;
+		//checking the age
+		if(age>18) {
+			System.out.print("Age is greater than 18");
+		}
+	}
+}
